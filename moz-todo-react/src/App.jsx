9068b86311c2cf7330a6e197cab26c8a9b1a6669
JoachimKvamme <kaj082024@gmail.com>
2024-10-09 14:56:40 +0200

    initial commit

---
+++ moz-todo-react/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const subject = "React!";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Hello, {subject}</h1>
+        <button type="button">Click me!</button>
+      </header>
+    </>
+  );
+}
+
+export default App;
